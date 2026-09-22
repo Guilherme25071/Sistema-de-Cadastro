69dc1464491105016f7c40cb10bb8038e1b29320
Guilherme25071 <guibasgodd@gmail.com>
2026-09-22 14:09:18 -0300

    Add files via upload

---
+++ SistemaCadastro/src/main/java/com/mycompany/sistemacadastro/Aluno.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemacadastro;
+
+/**
+ *
+ * @author guilherme62977766
+ */
+public class Aluno extends Cadastro{
+    
+    private int nota;
+    private double media;
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public Aluno(int nota, double media, String nome, int telefone, String endereco) {
+        super(nome, telefone, endereco);
+        this.nota = nota;
+        this.media = media;
+    }
+    
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ SistemaCadastro/src/main/java/com/mycompany/sistemacadastro/SistemaCadastro.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemacadastro;
+
+/**
+ *
+ * @author guilherme62977766
+ */
+public class SistemaCadastro {
+
+    public static void main(String[] args) {
+        Professor prof = new Professor (12000.00 , 12345 , "Marcio" , 9923 , "rua 12");
+       
+        Aluno aluno1 = new Aluno (8, 7.20, "Gui" , 23445 , "rua 20");
+        
+    
+    
+    
+    
+    }
+}
